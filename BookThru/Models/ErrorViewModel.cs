17e8b8d8978772e28768bc51a0916396ea9ae121
Michael <michaeljaison.me@gmail.com>
2019-01-13 19:15:25 -0500

    Add project files.

---
+++ BookThru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookThru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
