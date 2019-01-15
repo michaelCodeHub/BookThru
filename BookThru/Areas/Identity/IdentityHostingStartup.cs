d36111c82934863272818743319c3889c8c28b4e
Michael <michaeljaison.me@gmail.com>
2019-01-14 20:43:17 -0500

    Identity Scaffolded

---
+++ BookThru/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BookThru.Areas.Identity.Data;
+using BookThru.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookThru.Areas.Identity.IdentityHostingStartup))]
+namespace BookThru.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BookThruContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BookThruContextConnection")));
+
+                services.AddDefaultIdentity<BookThruUser>()
+                    .AddEntityFrameworkStores<BookThruContext>();
+            });
+        }
+    }
+}
